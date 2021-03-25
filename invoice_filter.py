c86486ecc0e94e7e97f83cd784cb302a93c7fefb
Peter-Newman Messan <p.messan@jacobs-university.de>
2021-03-25 19:07:19 +0100

    dummy csv, dtypes as str

--- invoice_filter.py
+++ invoice_filter.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 # read the data
 data = pd.read_csv("data/joined_dataset_final.csv")
-print(list(data))
+
+data["unique_id"] = ( data["WRBTR"].astype("str") + data["BUKRS"].astype("str") + data["BLDAT"] + data["XBLNR"] )
+# print(data["unique_id"].shape)
+# data.to_csv("data/unique_id.csv")
+print(data.iloc[0:1, 53])
