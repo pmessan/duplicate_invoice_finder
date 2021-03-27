7f3d7895f09abd16062eeb6d5089f3652f2aea02
Peter-Newman Messan <p.messan@jacobs-university.de>
2021-03-28 00:33:15 +0100

    runtime 3.0 on avg

--- src/invoice_filter.py
+++ src/invoice_filter.py
@@ -4,6 +4,7 @@ import pandas as pd
 # read the data
 data = pd.read_csv("../data/joined_dataset_final.csv")
 
+
 data["unique_id"] = ( data["WRBTR"].astype("str") + data["BUKRS"].astype("str") + data["BLDAT"] + data["XBLNR"] )
 # print(data["unique_id"].shape)
 # data.to_csv("data/unique_id.csv")
@@ -12,8 +13,11 @@ data["unique_id"] = ( data["WRBTR"].astype("str") + data["BUKRS"].astype("str") 
 
 # sub_data = data.iloc[0:30]
 
+# data.to_excel("../data/joined_dataset_final.xlsx")
+
 masterList = []
 duplicatesList = []
+dataFrames = []
 
 for i, row in data.iterrows():
     # print("Index: ", i)
@@ -22,15 +26,23 @@ for i, row in data.iterrows():
     else:   #if it's already there, we filter it out
         duplicatesList.append(row)
         
-print("Duplicates found: ", len(duplicatesList))
-# print(duplicatesList)
+# print("Duplicates found: ", len(duplicatesList))
+
 
 clms = list(data.columns)
 
-df = pd.DataFrame(columns=clms)
-for i in range(len(duplicatesList)):
-    df.loc[i] = duplicatesList[i]
+# df = pd.DataFrame(columns=clms)
+# for i in range(len(duplicatesList)):
+#     df.loc[i] = duplicatesList[i]
 
-df.to_excel("../data/duplicates.xlsx")
+# c = 1
+for i in duplicatesList:
+    # print("Duplicate " + str(c) + ": ")
+    # print(type(data["unique_id"]))
+    dataFrames.append(data.loc[data["unique_id"] == i["unique_id"]])
+    # c+=1
+dataFrames = pd.concat(dataFrames)
+# dataFrames.to_csv("../data/duplicates.csv")
+# dataFrames.to_excel("../data/duplicateDataFrames.xlsx")
 
-print(df.head(8))
+# print(df.head(8))
