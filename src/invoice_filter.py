7ac29fe1d98437e803ddde8f8ee0b8cd1080255a
Peter-Newman Messan <p.messan@jacobs-university.de>
2021-03-27 23:31:53 +0100

    reorginization

--- src/invoice_filter.py
+++ src/invoice_filter.py
@@ -7,68 +7,30 @@ data = pd.read_csv("../data/joined_dataset_final.csv")
 data["unique_id"] = ( data["WRBTR"].astype("str") + data["BUKRS"].astype("str") + data["BLDAT"] + data["XBLNR"] )
 # print(data["unique_id"].shape)
 # data.to_csv("data/unique_id.csv")
-# print(data["unique_id"].shape)
+# print(data.iloc[0:5, 53])
+# print(data.head())
 
-masterList = []     # store the different unique ids
+# sub_data = data.iloc[0:30]
 
-for r in data["unique_id"]:
-    if r not in masterList:
-        masterList.append(str(r))
+masterList = []
+duplicatesList = []
 
+for i, row in data.iterrows():
+    # print("Index: ", i)
+    if row["unique_id"] not in masterList:
+        masterList.append(row["unique_id"])
+    else:   #if it's already there, we filter it out
+        duplicatesList.append(row)
+        
+print("Duplicates found: ", len(duplicatesList))
+# print(duplicatesList)
 
-# print(len(masterList))
+clms = list(data.columns)
 
-for i in masterList:
-    print(masterList.index(i))
-    basket = data.loc[data["unique_id"] == i]
-    print(basket)
-    input()
-    # if basket.shape[1] > 1:
-    #     print("Duplicates found!")
-    # else:
-    #     print("No duplicates found!")
-    
-    
+df = pd.DataFrame(columns=clms)
+for i in range(len(duplicatesList)):
+    df.loc[i] = duplicatesList[i]
 
+df.to_excel("../data/duplicates.xlsx")
 
-
-# df.filter(like='bbi', axis=0)
-
-"""
-#Python program to remove duplicate elements in an array
-
-
-def remove_duplicate_elements(arr, n):
-
-if n == 0 or n == 1:
-return n
-
-temp = list(range(n))
-
-j = 0;
-for i in range(0, n-1):
-
-if arr[i] != arr[i+1]:
-temp[j] = arr[i]
-j += 1
-
-temp[j] = arr[n-1]
-j += 1
-
-for i in range(0, j):
-arr[i] = temp[i]
-
-return j
-
-
-n = int(input())
-sum = 0
-arr = []
-for i in range(0,n):
-temp = int(input())
-arr.append(temp)
-n = remove_duplicate_elements(arr,n)
-print(“Array after removing : “,end = “”)
-for i in range(0,n):
-print(arr[i], end = ” “)
-"""
+print(df.head(8))
